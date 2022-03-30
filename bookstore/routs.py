26e2be319338950291942d85fc1c7b95166acd2c
t-TeraFrost-t <60839173+t-TeraFrost-t@users.noreply.github.com>
2022-03-30 12:38:41 +0300

    Update routs.py

--- bookstore/routs.py
+++ bookstore/routs.py
@@ -436,10 +436,10 @@ def update_books(isbn):
         form.currency.default = book.currency.id
         form.description.default = book.description
         if form.validate_on_submit():
-            if not has_permition('update_books'):
-                    logger(user=session['Staff'].id,message= 'staff: '+session['Staff'].username+' tried to updated a book but has no permition')
-                    flash(f'no permition')
-                    return redirect('/backoffice-books') 
+           if not has_permition('update_books'):
+                logger(user=session['Staff'].id,message= 'staff: '+session['Staff'].username+' tried to updated a book but has no permition')
+                flash(f'no permition')
+                return redirect('/backoffice-books') 
            if form.description.data:
                 book.description = save_picture(form.description.data)
             book.autor=form.autor.data
