1e161243a9fa8266af7ffd9d39fa6d16664302f5
t-TeraFrost-t <60839173+t-TeraFrost-t@users.noreply.github.com>
2022-03-31 13:20:35 +0300

    Update __init__.py

--- bookstore/__init__.py
+++ bookstore/__init__.py
@@ -13,7 +13,7 @@ patch_psycopg()  # the only change
 
 
 app = Flask(__name__, template_folder='views')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://mincho:PALEsedem@localhost/testbookstore'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://hlanyiegpohmkh:8f5051602023d1893cfa28dcb25ec5f8875682740a6b68a458fff5a950c70e21@ec2-99-80-170-190.eu-west-1.compute.amazonaws.com:5432/divqiaop85jk0'
 app.config['SECRET_KEY'] = '7d32da10cecc362833c07714408b63bc'
 db = SQLAlchemy(app)
 engine = create_async_engine('postgresql+asyncpg://mincho:PALEsedem@localhost/testbookstore')
